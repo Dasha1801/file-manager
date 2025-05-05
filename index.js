63a706b833f9d77ed20f1e0875f622f474074f4c
Darya Belskaya <d.belskaya@team.globus-ltd.com>
2025-05-05 19:44:36 +0300

    feat: init project

---
+++ index.js
@@ -0,0 +1,36 @@
+import os from "os";
+import process from "process";
+import readline from "readline";
+
+const allArgs = process.argv.slice(2);
+const usernameMatch = allArgs.find((arg) => /^--username=/.test(arg));
+const username = usernameMatch
+  ? usernameMatch.replace(/^--username=/, "")
+  : "User";
+
+process.chdir(os.homedir());
+
+console.log(`Welcome to the File Manager, ${username}!`);
+console.log(`You are currently in ${process.cwd()}`);
+
+const cli = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: "Enter command: ",
+});
+
+cli.prompt();
+
+cli.on("line", async (line) => {
+  if (line.trim() === ".exit") {
+    cli.close();
+    return;
+  }
+
+  cli.prompt();
+});
+
+cli.on("close", () => {
+  console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+  process.exit(0);
+});
